8ade85257ec56107a8a39e5ab2e68bb9ec8bf1fa
Ladoxer <ladderdoxer@gmail.com>
2024-10-26 16:15:45 +0530

    initial commit

---
+++ lib/weather_screen.dart
@@ -0,0 +1,131 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:weather_app/additional_info_item.dart';
+import 'package:weather_app/hourly_forecast_item.dart';
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Weather App',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.refresh),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          // main card
+          SizedBox(
+            width: double.infinity,
+            child: Card(
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                  child: const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          '300°K',
+                          style: TextStyle(
+                              fontSize: 32, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 16),
+                        Icon(Icons.cloud, size: 64),
+                        SizedBox(height: 16),
+                        Text(
+                          'Rain',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          // weather forecast cards
+          const Text(
+            'Weather Forecast',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          const SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                HourlyForecastItem(
+                  time: '10:00',
+                  icon: Icons.cloud,
+                  temperature: '301.22',
+                ),
+                HourlyForecastItem(
+                  time: '10:00',
+                  icon: Icons.cloud,
+                  temperature: '301.22',
+                ),
+                HourlyForecastItem(
+                  time: '10:00',
+                  icon: Icons.cloud,
+                  temperature: '301.22',
+                ),
+                HourlyForecastItem(
+                  time: '10:00',
+                  icon: Icons.cloud,
+                  temperature: '301.22',
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          // additional information
+          const Text(
+            'Additional Information',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 12),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              AdditionalInfoItem(
+                icon: Icons.water_drop,
+                label: 'Humidity',
+                value: '60%',
+              ),
+              AdditionalInfoItem(
+                icon: Icons.wind_power,
+                label: 'Wind Speed',
+                value: '12 km/h',
+              ),
+              AdditionalInfoItem(
+                icon: Icons.thermostat,
+                label: 'Pressure',
+                value: '1000 hPa',
+              ),
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
